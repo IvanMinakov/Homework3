28056a53319be763642de954b27f33b5dba578d5
Ivan Minakov <i.minakov786@gmail.com>
2022-11-28 15:41:05 -0500

    merge

---
+++ Task3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int aaa = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=aaa; i++)
+{ 
+    System.Console.Write($"{i*i*i}, ");
+}
+System.Console.WriteLine();
